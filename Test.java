49551f4c4b3d63fd92f212273e094658fc715f1e
ChenZhang <ckcz123@126.com>
2017-03-06 18:22:46 +0800

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,18 @@
+/**
+ * Test JSON
+ * Created by ckcz123 on 2017/3/6.
+ */
+public class Test {
+    public static void main(String[] args){
+        String test="{ " +
+                "'int': 3, " +
+                "'double': -2.88 ,  " +
+                "'boolean':  false , " +
+                "'string' : 'abc +-*/\\%22 ',"+
+                "'list': ['test1', -33, 2.2, true, {'k':'v'}]," +
+                "'sub': {'int': 5, 'string': 'str', 'list':[]} , " +
+                " }";
+        JSON json=JSON.decode(test.replace('\'','"'));
+        System.out.println(JSON.toStringHumanReadable(json));
+    }
+}
